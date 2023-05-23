12da3cb51945a0d4895ec18a6bc78647906e19cc
tush87 <tusharj8788@gmail.com>
2023-05-23 20:17:58 +0530

    covert app conponent function to class and add state and add some functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,78 @@
 import './App.css';
+import React from 'react';
+// function App() {
+//   return (
+//     <div className="App">
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      box: "text",
+      showTooltip: false,
+      tooltipPostion: 'top'
+    }
+  }
+
+  // onover function
+  onHover = () => {
+    this.setState({
+      text: "image",
+      showTooltip: true
+    }, () => {
+      console.log(this.state);
+    })
+  }
+
+  // onLeave function
+  onLeave = () => {
+    this.setState({
+      text: "text",
+      showTooltip: false
+    }, () => {
+      console.log("onleave");
+    })
+  }
+
+  // handle postion chenge 
+  handleChange = (e) => {
+    this.setState({
+      tooltipPostion: e.target.value
+    }, () => {
+      console.log(this.state);
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          <select value={this.state.tooltipPostion} onChange={this.handleChange}>
+            <option value="top">top</option>
+            <option value="right">right</option>
+            <option value="left">left</option>
+            <option value="bottom">bottom</option>
+          </select>
+        </div>
+        <br/><br/><br/><br/>
+        {/* main */}
+        <div id='main' onMouseOver={this.onHover} onMouseLeave={this.onLeave}>
+            {(this.state.showTooltip && <div>hii</div>)}
+            {(this.state.box === "text" && <span>Touch me</span>)}
+            {(this.state.box === "" && <img
+              alt='PIC'
+              src=''
+            />)}
+        </div>
+      </>
+    )
+  }
+
 }
+
 
 export default App;
